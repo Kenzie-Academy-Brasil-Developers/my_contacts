274703c24fe029187d205aade767eaba24a25149
Jonatas Bacelar <129187557+JhonataBacellar@users.noreply.github.com>
2024-02-29 12:01:51 -0300

    documentationAPI

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,7 @@
 import 'reflect-metadata';
 import 'express-async-errors';
+import swaggerUiExpress  from 'swagger-ui-express';
+import swaggerDocument from './swagger.json';
 import express, {Application, json} from 'express';
 import { routes } from './routers';
 import { handleErrors } from './middlewares/handleErrors.middleware';
@@ -8,6 +10,7 @@ export const app: Application = express()
 
 app.use(json())
 
+app.use('/api-documentation', swaggerUiExpress.serve, swaggerUiExpress.setup(swaggerDocument))
 app.use('/', routes)
 
 app.use(handleErrors)
